7070c270ad388ec23ef2bd36be25f31ee14938b3
Elliot Saba <staticfloat@gmail.com>
2017-08-19 20:53:07 -0700

    EVEN MORE SASS

--- app/brayerpot.py
+++ app/brayerpot.py
@@ -133,7 +133,7 @@ def get_db():
 def handle_help(payload):
     """
     Given a `@prayerbot help`, we will send them a direct message explaining
-    our raison d'existence.
+    our raison d'existence.  That one's for you, @yaup
     """
 
     help_msg = """
@@ -141,17 +141,13 @@ Hello There!  My name is `@prayerbot`, and I exist to help organize prayers thro
 
 -  `help`: Print out this help message.  You just did this, so you probably don't need me to tell you how to do it again.
 
-
 -  `signup <group>`: Sign up to be a part of prayer group `<group>`. Example:
 > *@prayerbot signup LWGuys*
 This will put you into the prayer rotations for the LW guys prayer group, where you will be randomly matched with other guys each week to pray for eachother.
 
-
 -  `stop <group>`: The reverse of the above command; if you don't want to do this anymore, this will take you out of a group you previously signed up for.
 
-
 -  `stop`: If you don't give me a group name, I'll just remove you from all groups you were a part of.
-
 
 -  `list`: List all groups you're in.
     """
